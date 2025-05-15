bcc2a258819e5a333c21b9fb4a0fa6f739d7da7c
MuhammadRadzi <nippon.pein14@gmail.com>
2025-05-15 14:02:13 +0800

    Tugas Array ForEach

---
+++ tugas12_Ridho.php
@@ -0,0 +1,7 @@
+<?php
+
+$MerekSepatu = ["Aerostreet","Compass","Ortus","Specs"];
+echo "Daftar Koleksi Sepatu: <br> ";
+foreach ($MerekSepatu as $DaftarSepatu) {
+    echo "- " .$DaftarSepatu ."<br>";
+}
